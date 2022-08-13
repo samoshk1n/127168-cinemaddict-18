a47fa509fa788da859c7533dbf625ce68d9cf6ba
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-08-13 15:19:03 +0400

    Исправлена разметка кнопки

--- src/view/show-more-button-view.js
+++ src/view/show-more-button-view.js
@@ -1,11 +1,6 @@
 import {createElement} from '../render.js';
 
-const createShowMoreButtonTemplate = () => (
-  `<section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container"></div>
-  </section>`
-);
+const createShowMoreButtonTemplate = () => '<button class="films-list__show-more">Show more</button>';
 
 export default class ShowMoreButtonView {
   getTemplate() {
