439376a1a8a99d8c28d32711a2120a0151278bdc
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-08-16 19:39:44 +0400

    Описаны задачи в комментариях

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,33 +1,33 @@
 export const generateFilm = (id) => ({
-  id,
-  comments: [],
+  id, // TODO Сделать генерацию айдишников;
+  comments: [], // TODO Сгенерировать айдишники комментариев 0-5 шт. Главная и попап
   filmInfo: {
-    title: 'A Little Pony Without The Carpet',
-    alternativeTitle: 'Laziness Who Sold Themselves',
-    totalRating: 5.3,
-    poster: 'images/posters/blue-blazes.jpg',
-    ageRating: 0,
-    director: 'Tom Ford',
+    title: 'A Little Pony Without The Carpet', // TODO Выбор из произвольного массива. Главная и попап
+    alternativeTitle: 'Laziness Who Sold Themselves', // TODO Для рыбы подставим из основного title. Попап
+    totalRating: 5.3, // TODO Генерация случайного числа. Главная и попап
+    poster: 'images/posters/blue-blazes.jpg', // TODO Выбор из массива адрессов. Главная и попап
+    ageRating: 0, // TODO Выбор из произвольного массива. Попап
+    director: 'Tom Ford', // TODO Выбор из произвольного массива. Попап
     writers: [
       'Takeshi Kitano'
-    ],
+    ], // TODO Выбор из произвольного массива несколько значений (1-3). Обработка writer(s). Попап
     actors: [
       'Morgan Freeman'
-    ],
+    ], // TODO Выбор из произвольного массива несколько значений (3-7). Попап
     release: {
-      'date': '2019-05-11T00:00:00.000Z',
-      'release_country': 'Finland'
+      'date': '2019-05-11T00:00:00.000Z', // TODO Генерация случайных дат. Главная и попап. На главной только год, на попапе "день месяц год"
+      'release_country': 'Finland' // TODO Выбор из произвольного массива. Попап
     },
-    runtime: 77,
+    runtime: 77, // TODO Генерация случайного числа. Главная и попап. Формат "часы минуты"
     genre: [
       'Comedy'
-    ],
-    description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
+    ], // TODO Выбор из произвольного массива несколько значений (1-3). Обработка genre(s) на попапе. Главная и попап
+    description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.' // TODO Выбор из произвольного массива. Главная и попап. На главной не больше 140 символов
   },
   userDetails: {
-    watchlist: false,
-    alreadyWatched: true,
-    watchingDate: '2019-04-12T16:12:32.554Z',
-    favorite: false
+    watchlist: false, // TODO Генерация случайного булева значения
+    alreadyWatched: true, // TODO Генерация случайного булева значения
+    watchingDate: '2019-04-12T16:12:32.554Z', // TODO Генерация случайной даты
+    favorite: false // TODO Генерация случайного булева значения
   }
 });
