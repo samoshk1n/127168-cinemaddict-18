0d8f46a34c5a42ee7582b8e638ed44d5ff3c6915
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-08-20 23:37:36 +0400

    Модули подключаются в алфавитном порядке

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
-import ProfilePresenter from './presenter/profile-presenter.js';
-import NavigationPresenter from './presenter/navigation-presenter.js';
-import SortPresenter from './presenter/sort-presenter.js';
 import FilmsPresenter from './presenter/films-presenter.js';
-import StatisticsPresenter from './presenter/statistics-presenter.js';
+import NavigationPresenter from './presenter/navigation-presenter.js';
 import PopupPresenter from './presenter/popup-presenter.js';
-import FilmsModel from './model/films-model.js';
+import ProfilePresenter from './presenter/profile-presenter.js';
+import SortPresenter from './presenter/sort-presenter.js';
+import StatisticsPresenter from './presenter/statistics-presenter.js';
+
 import CommentsModel from './model/comments-model.js';
+import FilmsModel from './model/films-model.js';
 
 const siteBodyElement = document.querySelector('body');
 const siteHeaderElement = siteBodyElement.querySelector('.header');
 const siteMainElement = siteBodyElement.querySelector('.main');
 const statisticElement = siteBodyElement.querySelector('.footer__statistics');
 
-const profilePresenter = new ProfilePresenter();
-const navigationPresenter = new NavigationPresenter();
-const sortPresenter = new SortPresenter();
 const filmsPresenter = new FilmsPresenter();
-const statisticsPresenter = new StatisticsPresenter();
+const navigationPresenter = new NavigationPresenter();
 const popupPresenter = new PopupPresenter();
-const filmsModel = new FilmsModel();
+const profilePresenter = new ProfilePresenter();
+const sortPresenter = new SortPresenter();
+const statisticsPresenter = new StatisticsPresenter();
 const commentsModel = new CommentsModel();
+const filmsModel = new FilmsModel();
 
 profilePresenter.init(siteHeaderElement);
 navigationPresenter.init(siteMainElement);
