b224b10a51fc80f471f5d2fcc2b5eecd467693e0
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-09-21 01:52:45 +0400

    Реализована динамичная отрисовка компонента при помощи объекта состояния

--- src/view/new-comment-view.js
+++ src/view/new-comment-view.js
@@ -1,40 +1,55 @@
 import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
+import {EMOTIONS} from '../const.js';
 
-const createNewCommentTemplate = () => (
+const START_STATEFUL = {
+  comment: '',
+  emotion: null
+};
+
+const checkEmotion = (emotion, state) => emotion === state ? 'checked' : '';
+
+const createEmotionInputs = (state) => EMOTIONS.reduce((acc, emotion) => (
+  `${acc}
+  <input class="film-details__emoji-item visually-hidden"
+    name="comment-emoji"
+    type="radio"
+    id="emoji-${emotion}"
+    value="${emotion}"
+    ${checkEmotion(emotion, state.emotion)}>
+  <label class="film-details__emoji-label" for="emoji-${emotion}">
+    <img src="./images/emoji/${emotion}.png" width="30" height="30" alt="emoji">
+  </label>`
+), '');
+
+const createEmotionLabel = (state) => (
+  state.emotion ? `<img src="./images/emoji/${state.emotion}.png" width="55" height="55" alt="${state.emotion}">` : ''
+);
+
+const createNewCommentTemplate = (state) => (
   `<form class="film-details__new-comment" action="" method="get">
-    <div class="film-details__add-emoji-label"></div>
+    <div class="film-details__add-emoji-label">
+      ${createEmotionLabel(state)}
+    </div>
 
     <label class="film-details__comment-label">
-      <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+      <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${state.comment}</textarea>
     </label>
 
     <div class="film-details__emoji-list">
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
-      <label class="film-details__emoji-label" for="emoji-smile">
-        <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
-      <label class="film-details__emoji-label" for="emoji-sleeping">
-        <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
-      <label class="film-details__emoji-label" for="emoji-puke">
-        <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
-      <label class="film-details__emoji-label" for="emoji-angry">
-        <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
-      </label>
+      ${createEmotionInputs(state)}
     </div>
   </form>`
 );
 
 export default class NewCommentView extends AbstractStatefulView {
+  constructor(state = START_STATEFUL) {
+    super();
+
+    this._state = state;
+  }
+
   get template() {
-    return createNewCommentTemplate();
+    return createNewCommentTemplate(this._state);
   }
 
   static parseCommentToState = (comment) => ({...comment,
