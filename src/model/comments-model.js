95e58712dbc6e51a1675bb7447fa94eb442d941d
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-10-05 02:45:26 +0400

    В модель комментариев добавлены методы добавления и удаления комментариев
    Так как моковые данные создали случайно комментарии и в модели фильмов также созданы случайным образом IDшники, то deleteComment работает сейчас некорректно. Его можно проверить только при добавлении собственных комментариев

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,11 +1,36 @@
 import {generateComment} from '../mock/comment.js';
 import {NUMBER_OF_COMMENTS} from '../const.js';
+import Observable from '../framework/observable.js';
+import {nanoid} from 'nanoid';
 
+const USER_NAME = 'Dima Samoshkin';
 
-export default class CommentsModel {
+export default class CommentsModel extends Observable {
   #comments = Array.from({length: NUMBER_OF_COMMENTS}, (_value, index) => generateComment(index));
 
   get comments () {
     return this.#comments;
   }
+
+  addComment = (updateType, update) => {
+    const newComment = {
+      ...update,
+      id: nanoid(),
+      author: USER_NAME,
+      date: new Date()
+    };
+
+    this.#comments.push(newComment);
+
+    this._notify(updateType, update);
+  };
+
+  deleteComment = (currentID) => {
+    const currentComment = this.#comments.find((comment) => comment.id === currentID);
+    const commentIndex = this.comments.indexOf(currentComment);
+
+    if (commentIndex !== -1) {
+      this.comments.splice(commentIndex, 1);
+    }
+  };
 }
