ccbba53f780c4bca8efda984d35b1b5c82c4ae47
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-10-05 16:28:42 +0400

    Реализован метотд адартер
    В films-model структура данных с сервера подогнана под структуру данных модели, в films-api-service произведена обратная процедура

--- src/films-api-service.js
+++ src/films-api-service.js
@@ -11,12 +11,47 @@ export default class FilmsApiService extends ApiService {
     const response = await this._load({
       url: `movies/${film.id}`,
       method: HTTP_METHOD.PUT,
-      body: JSON.stringify(film),
+      body: JSON.stringify(this.#adaptToServer(film)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (film) => {
+    const adaptedFilm = {...film,
+      'film_info': {
+        ...film['filmInfo'],
+        'alternative_title': film['filmInfo']['alternativeTitle'],
+        'total_rating': film['filmInfo']['totalRating'],
+        'age_rating': film['filmInfo']['ageRating'],
+        'genre': film['filmInfo']['genres'],
+        release: {
+          ...film['filmInfo']['release'],
+          'release_country': film['filmInfo']['release']['releaseСountry']
+        }
+      },
+
+      'user_details': {
+        ...film['userDetails'],
+        'already_watched': film['userDetails']['alreadyWatched'],
+        'watching_date': film['userDetails']['watchingDate']
+      }
+    };
+
+    delete adaptedFilm['filmInfo'];
+    delete adaptedFilm.filmInfo['alternativeTitle'];
+    delete adaptedFilm.filmInfo['totalRating'];
+    delete adaptedFilm.filmInfo['ageRating'];
+    delete adaptedFilm.filmInfo['genres'];
+    delete adaptedFilm.filmInfo.release['releaseСountry'];
+
+    delete adaptedFilm['userDetails'];
+    delete adaptedFilm.userDetails['alreadyWatched'];
+    delete adaptedFilm.userDetails['watchingDate'];
+
+    return adaptedFilm;
   };
 }
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -11,10 +11,44 @@ export default class FilmsModel extends Observable {
     this.#filmsApiService = filmsApiService;
 
     this.#filmsApiService.films.then((films) => {
-      console.log(films);
-
+      console.log(films.map(this.#adaptToClient)[0]);
     });
   }
+
+  #adaptToClient = (film) => {
+    const adaptedFilm = {...film,
+      filmInfo: {
+        ...film['film_info'],
+        alternativeTitle: film['film_info']['alternative_title'],
+        totalRating: film['film_info']['total_rating'],
+        ageRating: film['film_info']['age_rating'],
+        genres: film['film_info']['genre'],
+        release: {
+          ...film['film_info']['release'],
+          releaseСountry: film['film_info']['release']['release_country']
+        }
+      },
+
+      userDetails: {
+        ...film['user_details'],
+        alreadyWatched: film['user_details']['already_watched'],
+        watchingDate: film['user_details']['watching_date']
+      }
+    };
+
+    delete adaptedFilm['film_info'];
+    delete adaptedFilm.filmInfo['alternative_title'];
+    delete adaptedFilm.filmInfo['total_rating'];
+    delete adaptedFilm.filmInfo['age_rating'];
+    delete adaptedFilm.filmInfo['genre'];
+    delete adaptedFilm.filmInfo.release['release_country'];
+
+    delete adaptedFilm['user_details'];
+    delete adaptedFilm.userDetails['already_watched'];
+    delete adaptedFilm.userDetails['watching_date'];
+
+    return adaptedFilm;
+  };
 
   get films () {
     return this.#films;
