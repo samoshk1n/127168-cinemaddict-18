d845f9da1a663f6980667878ea247866f378ba34
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-10-09 00:15:22 +0400

    Исправлено расположение кнопки showMoreButton

--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -139,7 +139,7 @@ export default class FilmsPresenter {
     if (filmCount > this.#renderedFilmCount) {
       this.#showMoreButtonComponent = new ShowMoreButtonView();
       this.#showMoreButtonComponent.setShowMoreButtonClickHandler(this.#onShowMoreButtonClick);
-      render(this.#showMoreButtonComponent, this.#filmsListComponent.filmsListContainer);
+      render(this.#showMoreButtonComponent, this.#filmsListComponent.element);
     }
   };
 
